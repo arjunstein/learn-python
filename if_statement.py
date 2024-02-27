9e7fcf1eca22b0c897a328d35179ee74c815b397
arjunstein <arjun.gnw09@gmail.com>
2024-02-27 08:57:12 +0700

    ⚡️ learn if statement

---
+++ if_statement.py
@@ -0,0 +1,13 @@
+# Belajar if
+
+tentara = 10
+perompak = 10
+
+if tentara < perompak:
+    print("Tetap lawan dan tak gentar")
+
+if tentara > perompak:
+    print("Kamu menang!")
+
+if  tentara == perompak:
+    print("satu lawan satu")
