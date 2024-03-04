02d1e128507e38762041f681ef5ae8bd99ea7cc2
arjunstein <arjun.gnw09@gmail.com>
2024-03-04 09:12:04 +0700

    👔 learn method parameter

---
+++ method_parameter.py
@@ -0,0 +1,12 @@
+# belajar method parameter 
+
+def  greet(name):
+    print(f"Hello, {name} ")
+    
+greet("Arjun")
+greet("Gunawan")
+
+def greet(firstName, lastName):
+    print(f"Hello, {firstName} {lastName}")
+
+greet("Arjun","Gunawan")
