7a5fdede6be4de2259b9f06a0aa8967f691c54b6
arjunstein <arjun.gnw09@gmail.com>
2024-03-04 09:00:39 +0700

    ✨ learn method

---
+++ method_learn.py
@@ -0,0 +1,14 @@
+# belajar membuat method / function
+
+nama = []
+def print_nama():
+    print("=========")
+    for data in nama:
+        print(data)
+
+nama.append("Arjun")
+print_nama()
+nama.append("Gunawan")
+print_nama()
+nama.append("Aresha")
+print_nama()
